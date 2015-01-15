c9159f2ae555156e922760bde0b58c7170f48a7d
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-01-15 14:02:24 +0000

    New question flow for fee-remission eligibility.

--- lib/smartdown_plugins/fee-remission-eligibility/render_time.rb
+++ lib/smartdown_plugins/fee-remission-eligibility/render_time.rb
@@ -4,39 +4,66 @@ require 'smartdown/model/answer/date'
 module SmartdownPlugins
   module FeeRemissionEligibility
 
-    def self.your_partner(relationship)
-      if relationship == 'couple'
-        ' and your partner'
-      end
-    end
-
-    def self.disposable_capital_threshold(under_age_threshold, claim_fee=nil)
-      if under_age_threshold
-        fee = claim_fee.to_f
-        if fee <= 1000
-          '3,000'
-        elsif fee > 1000 && fee <= 1335
-          '4,000'
-        elsif fee > 1335 && fee <= 1665
-          '5,000'
-        elsif fee > 1665 && fee <= 2000
-          '6,000'
-        elsif fee > 2000 && fee <= 2330
-          '7,000'
-        elsif fee > 2330 && fee <= 4000
-          '8,000'
-        elsif fee > 4000 && fee <= 5000
-          '10,000'
-        elsif fee > 5000 && fee <= 6000
-          '12,000'
-        elsif fee > 6000 && fee <= 7000
-          '14,000'
-        elsif fee > 7000
-          '16,000'
+    class << self
+      def your_partner(relationship)
+        if relationship == 'couple'
+          ' and your partner'
         end
-      else
-        '16,000'
       end
+
+      def your_partners(relationship)
+        if relationship == 'couple'
+          " and your partner's"
+        end
+      end
+
+      def disposable_capital_threshold(under_age_threshold, fee=nil)
+
+        threshold = if under_age_threshold == 'no'
+                      16000
+                    else
+                      if fee <= 1000
+                        3000
+                      elsif fee > 1000 && fee <= 1335
+                        4000
+                      elsif fee > 1335 && fee <= 1665
+                        5000
+                      elsif fee > 1665 && fee <= 2000
+                        6000
+                      elsif fee > 2000 && fee <= 2330
+                        7000
+                      elsif fee > 2330 && fee <= 4000
+                        8000
+                      elsif fee > 4000 && fee <= 5000
+                        10000
+                      elsif fee > 5000 && fee <= 6000
+                        12000
+                      elsif fee > 6000 && fee <= 7000
+                        14000
+                      elsif fee > 7000
+                        16000
+                      end
+                    end
+
+        Smartdown::Model::Answer::Money.new(threshold).humanize
+      end
+
+      def monthly_income_threshold(relationship, dependent_children=nil)
+        threshold = if relationship == 'couple'
+                      1245
+                    else
+                      1085
+                    end
+
+        if dependent_children
+          number_of_children = dependent_children.to_i
+          adjustment = (number_of_children * 245)
+          threshold += adjustment
+        end
+
+        Smartdown::Model::Answer::Money.new(threshold).humanize
+      end
+
     end
   end
 end
