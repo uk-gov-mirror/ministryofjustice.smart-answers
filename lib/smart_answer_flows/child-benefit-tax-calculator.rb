a88d00458632db8b5c486e6dc8dd246701160cdb
Becca <rebecca.pearce.92@gmail.com>
2020-08-02 19:39:16 +0100

    Add Question 1 to logic flow
    Add question to enter the number of children the claim is being made for

--- lib/smart_answer_flows/child-benefit-tax-calculator.rb
+++ lib/smart_answer_flows/child-benefit-tax-calculator.rb
@@ -1,12 +1,17 @@
 module SmartAnswer
   class ChildBenefitTaxCalculatorFlow < Flow
     def define
-      name 'child-benefit-tax-calculator'
+      name "child-benefit-tax-calculator"
       start_page_content_id "201fff60-1cad-4d91-a5bf-d7754b866b87"
       flow_content_id "26f5df1d-2d73-4abc-85f7-c09c73332693"
       status :draft
 
-      value_question :question_1? do
+      # Q1
+      value_question :how_many_children?, parse: Integer do
+        on_response do |response|
+          self.calculator = Calculators::ChildBenefitTaxCalculator.new
+          calculator.children_count = response.to_i
+        end
         next_node do
           outcome :outcome_1
         end
