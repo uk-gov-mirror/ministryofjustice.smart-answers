6fca2faa29fa4b97061c4e483c41ba6b53ecd3e6
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-03-16 13:43:07 +0000

    Pre-populate radio button questions in session flow
    The session flow stores user responses in a session. The session can only be accessed
    from controllers and views. This is a problem because the code to determine which
    radio button was selected is in a helper nested in a presenter, so we need to pass the
    relevant value from the session to that helper.
    
    We tried passing in the whole session, but that doesn't work, because even though the
    session looks like a hash, it is an object and cannot be serialised.
    
    The radio button partial is used by both types of smart answer flow, not just sessions,
    so not all of the values we need are available (i.e. params[:node_slug]) so the querying
    of the session has been put inside a guard so the other smart-answers still work.

--- test/integration/revisit_page_test.rb
+++ test/integration/revisit_page_test.rb
@@ -26,6 +26,28 @@ class RevisitPageTest < ActionDispatch::SystemTestCase
     end
   end
 
+  test "back link returns previously visited page with prefilled radio button answer" do
+    visit "find-coronavirus-support/s"
+    within "legend" do
+      assert_page_has_content "What do you need help with because of coronavirus?"
+    end
+
+    check("Getting food or medicine", visible: false)
+    click_on "Continue"
+    assert_page_has_content "Getting food"
+    choose("Yes", visible: false)
+    click_on "Continue"
+    assert_page_has_content "Getting food or medicine"
+    click_on "Back"
+    assert_page_has_content "Getting food"
+
+    checked_button = find(:css, "[value='yes']", visible: false)
+    unchecked_button = find(:css, "[value='no']", visible: false)
+
+    assert_equal(checked_button.checked?, true)
+    assert_equal(unchecked_button.checked?, false)
+  end
+
   def assert_page_has_content(text)
     assert page.has_content?(text), "'#{text}' not found in page"
   end
